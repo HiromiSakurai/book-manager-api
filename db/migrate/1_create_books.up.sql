f199bbbaa7595c494e79607f829445d2b8434e41
HiromiSakurai <sakurai465270@gmail.com>
2021-11-25 10:18:12 +0900

    Add books table

---
+++ db/migrate/1_create_books.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS book_manager_api.books(
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
